4464654dc124d3a4c9dfe86847098297798dc3f5
Ictor <ictormorrison@gmail.com>
2015-08-07 11:38:51 +0100

    gives advice based on how many points left for traits

--- Program.java
+++ Program.java
@@ -25,6 +25,20 @@ public class Program {
 		System.out.println("Awareness:" + awareness + " Coordination:" + coordination + " Ingenuity:" + ingenuity + " Presence:" + presence + " Resolve:" + resolve + " Strength:" + strength);
 		System.out.println("Attributes Total:" + total);
 		System.out.println("You have " + pointsleft + " Character Points left for traits. It's recommended you spend 18 points on attributes and 6 on traits.");
+		
+		if(pointsleft < 0){
+			System.out.println("WARNING: You have used too many character points. Lower your attributes or add negative traits.");
+		}
+		else if(pointsleft >= 0 && pointsleft < 6){
+			System.out.println("You've spent a few more points than recommended, but that's okay. :)");
+		}
+		else if(pointsleft > 6){
+			System.out.println("You can afford to spend more points! Up your attributes, or have loads left for traits.");
+		}
+		else if(pointsleft == 6){
+			System.out.println("Well done! You've spent your points exactly as recommended. :D");
+		}
+		
 	}
 
 }
